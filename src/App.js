aea1f95e101541f7c1aa3fbe99f0f0d8bf571aef
bmbx08 <bmbx08@bu.ac.kr>
2024-12-08 03:32:50 +0900

    added name files, added simple page routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import LoginPage from './pages/KichanPages/LoginPage/LoginPage';
 import './App.css';
 
+
 function App() {
+  const [page,setPage] = useState("login");
+
   return (
     <div>
-
+      {page==="login"&&<LoginPage/>}
     </div>
   );
 }
---
+++ src/pages/KichanPages/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./LoginPage.style.css"
+
+const LoginPage = () => {
+  return (
+    <div className='login-page'>
+        LoginPage
+    </div>
+  )
+}
+
+export default LoginPage
